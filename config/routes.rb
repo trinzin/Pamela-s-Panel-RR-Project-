41ca740ab69ffeae76ba21aa5de44eda9b719f5f
TRinzin <trinzin@nyc.yearup.org>
2019-01-09 16:35:08 -0500

    keep striving

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'posts#index'
+  Rails.application.routes.draw do
+
+  get 'post/index'
+
+  get 'about' => 'pages#about'
+ 
 end
